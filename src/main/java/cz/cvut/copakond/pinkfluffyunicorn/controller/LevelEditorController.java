daeeaa29325b80c22c6aa48c6a13794e3ba22c55
Ondrej Copak <copakond@fel.cvut.cz>
2025-03-28 16:53:25 +0100

    moved from controller into view

--- src/main/java/cz/cvut/copakond/pinkfluffyunicorn/controller/LevelEditorController.java
+++
@@ -1,4 +0,0 @@
-package cz.cvut.copakond.pinkfluffyunicorn.controller;
-
-public class LevelEditorController {
-}
---
+++ src/main/java/cz/cvut/copakond/pinkfluffyunicorn/view/ProfileController.java
@@ -0,0 +1,4 @@
+package cz.cvut.copakond.pinkfluffyunicorn.view;
+
+public class ProfileController {
+}
